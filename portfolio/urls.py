da1b3c889970cdf9692a97ad56b84a59a63742fe
Rahul Gummula <rahulgummula9@gmail.com>
2024-12-21 01:11:51 +0530

    Sixth commit

--- portfolio/urls.py
+++ portfolio/urls.py
@@ -15,17 +15,18 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
 from app import views
 from django.conf import settings
 from django.conf.urls.static import static
+from django.urls import path, include, re_path
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns 
 
-
+from django.views.static import serve
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',include("app.urls")),
-    
+    re_path(r'^static/(?P<path>.*)$', serve, {'document_root': settings.STATIC_ROOT}),
 
 ]
 if settings.DEBUG:
